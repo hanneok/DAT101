9a1791cd999069b0375421c3dfd511e13d6cde57
Hanne Odden Kaasa <hanne-odden-kaasa@hotmail.com>
2025-09-15 14:19:59 +0200

    2.2

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,18 +4,20 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const part1Ans = 2 + 3 * (2 - 4) * 6;
+printOut("2 + 3 * 2 - 4 * 6 = " + part1Ans);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const millimeters = (34 + (25 * 100)) * 10;
+const inches = millimeters / 25.4;
+printOut("(34 + (25 * 100)) * 10. Then divided by 25.4 = " + inches.toFixed(2));
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+let sekunder = 45 / 60;
+let Timer = 12 + (3 * 24); 
+let minutter = Timer * 60 + 14 + sekunder;
+printOut(minutter);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
